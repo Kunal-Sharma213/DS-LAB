b36dc51ee0536c45239f3890c9ed4a77b01ae6b0
Kunal-Sharma213 <68760164+Kunal-Sharma213@users.noreply.github.com>
2021-09-30 15:44:15 +0530

    Minor changes

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -254,26 +254,6 @@ int getMax(int arr[], int n)
     return mx;
 }
 
-// void Node ::countSort(int arr[], int n, int exp)
-// {
-//     int output[n]; // output array
-//     int i, count[10] = {0};
-
-//     for (i = 0; i < n; i++)
-//         count[(arr[i] / exp) % 10]++;
-
-//     for (i = 1; i < 10; i++)
-//         count[i] += count[i - 1];
-
-//     for (i = n - 1; i >= 0; i--)
-//     {
-//         output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-//         count[(arr[i] / exp) % 10]--;
-//     }
-
-//     for (i = 0; i < n; i++)
-//         arr[i] = output[i];
-// }
 void Node ::radixSort(int *arr, int n, int digits)
 {
     int i, j, m, p = 1, index, temp, count = 0;
